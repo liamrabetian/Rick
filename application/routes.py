66f6fa6420c9215326bfb61e133e97e75aa6a62d
Erfan-kasaei <erfan.k008@gmail.com>
2019-12-25 12:16:34 +0330

    i added error handling code

--- application/routes.py
+++ application/routes.py
@@ -2,6 +2,10 @@ from flask import render_template
 from application import app
 from application.models import User,Post
 
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
 @app.route('/')
 @app.route('/index')
 def index():
